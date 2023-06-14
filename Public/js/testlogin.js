e5acf3e13e2dfa564b5b3f174c5848cb74d584ac
Jishnu Devarapalli <srijishnu.devarapalli@student.allenisd.org>
2023-06-14 12:20:51 -0500

    made js file in function format

--- Public/js/testlogin.js
+++ Public/js/testlogin.js
@@ -1,43 +1,33 @@
-const loginForm = document.getElementById("login-form");
-const loginButton = document.getElementById("login-form-submit");
-const loginErrorMsg = document.getElementById("login-error-msg");
+const notificationButton = document.getElementById("notification-button");
 
-loginButton.addEventListener("click", (e) => {
-    e.preventDefault();
-    const email = loginForm.username.value;
-    const password = loginForm.password.value;
+//doing something on click
+function testEmail() {
 
-    sendLogin(email, password);
+    notificationButton.addEventListener("click", async () => {
+	const email = prompt("Enter your email address:");
 
-})
+	if (email) {
+	    try {
+		const response = await fetch("/send-notification", {
+		    method: 'POST',
+		    headers: {
+			'Content-Type': 'application/json'
+		    },
+		    body: JSON.stringify({ email })
+		});
 
-async function sendLogin(email, password) {
-    var details = JSON.stringify({username: email, password: password});
-    var formBody = new URLSearchParams();
-    formBody.append('username', email);
-    formBody.append('password', password);
-    const response = await fetch("./login", {
-	method: 'POST',
-	headers: {
-	    'Accept': 'application/json',
-	    'Content-Type': 'application/x-www-form-urlencoded'
-	},
-	body: formBody.toString()
-    });
-
-    console.log(response);
-
-    response.json().then(data => {
-	//const alertString = JSON.parse(data);
-	//console.log(data)
-	if (data.reason){
-	    alert(data.reason);
-	}
-	else if (data.error == "Success"){
-	    window.location.href = "./classes";
-	}
-	else{
-	    alert(data.error);
+		if (response.ok) {
+		    alert("Notification sent successfully!");
+		} else {
+		    alert("Failed to send notification.");
+		}
+	    } catch (error) {
+		alert("An error occurred while sending the notification.");
+		console.error(error);
+	    }
 	}
     });
 }
+
+
+
