f2e7b9b2f6c78836d36b193b86e396e631c953e7
Jane Williams <Pranav.krishnan@allenisd.org>
2023-06-14 10:59:26 -0500

    Added js test file for testing rest api

---
+++ Public/js/testlogin.js
@@ -0,0 +1,26 @@
+const notificationButton = document.getElementById("notification-button");
+
+notificationButton.addEventListener("click", async () => {
+    const email = prompt("Enter your email address:");
+
+    if (email) {
+	try {
+	    const response = await fetch("/send-notification", {
+		method: 'POST',
+		headers: {
+		    'Content-Type': 'application/json'
+		},
+		body: JSON.stringify({ email })
+	    });
+
+	    if (response.ok) {
+		alert("Notification sent successfully!");
+	    } else {
+		alert("Failed to send notification.");
+	    }
+	} catch (error) {
+	    alert("An error occurred while sending the notification.");
+	    console.error(error);
+	}
+    }
+});
