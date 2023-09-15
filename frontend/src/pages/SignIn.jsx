1703471097f64f2376691a86c20eef64f5b3c19e
Brett Kaplan <bkapla01@gmail.com>
2023-09-15 16:22:50 -0500

    added login request

--- frontend/src/pages/SignIn.jsx
+++ frontend/src/pages/SignIn.jsx
@@ -1,7 +1,40 @@
+import Button from 'react-bootstrap/Button';
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faGoogle } from '@fortawesome/free-brands-svg-icons';
 
 function SignIn() {
+
+    const [email, setEmail] = useState('')
+    const [password, setPassword] = useState('')
+    
+    const sendLogin = async (e) => {
+	e.preventDefault();
+	
+	const user = document.getElementById("signInEmail").value,
+	      pass = document.getElementById("signInPassword").value;
+	const formBody = new URLSearchParams();
+	formBody.append('username', user);
+	formBody.append('password', pass);
+	const response = await fetch("./login", {
+	    method: 'POST',
+	    headers: {
+		'Accept': 'application/json',
+		'Content-Type': 'application/x-www-form-urlencoded'
+	    },
+	    body: formBody.toString()
+	});
+
+	response.json().then(data => {
+	    if (data.reason) {
+		alert(data.reason);
+	    } else if (data.error == "Success") {
+		window.location.href = process.env.PUBLIC_URL + "/dashboard";
+	    } else {
+		alert(data.error);
+	    }
+	});
+    }
+    
     return (
         <section className="fullPage d-flex align-items-center" style={{ backgroundColor: "hsl(0, 0%, 96%)" }}>
             <div className="w-100 px-4 py-5 px-md-5 text-center text-lg-start">
@@ -23,42 +56,35 @@ function SignIn() {
                         <div className="col-lg-6 mb-5 mb-lg-0">
                             <div className="card">
                                 <div className="card-body py-5 px-md-5">
-                                    <form>
-                                        {/* <div className="row">
-                                            <div className="col-md-6 mb-4">
-                                                <div className="form-outline">
-                                                    <input type="text" id="form3Example1" className="form-control" />
-                                                    <label className="form-label" htmlFor="form3Example1">First name</label>
-                                                </div>
-                                            </div>
-                                            <div className="col-md-6 mb-4">
-                                                <div className="form-outline">
-                                                    <input type="text" id="form3Example2" className="form-control" />
-                                                    <label className="form-label" htmlFor="form3Example2">Last name</label>
-                                                </div>
-                                            </div>
-                                        </div> */}
-
+                                    <form
+					onSubmit={sendLogin}
+				    >
                                         <div className="form-outline mb-4">
-                                            <input type="email" id="signInEmail" className="form-control" />
+                                            <input type="email" id="signInEmail" className="form-control" value={email} onChange={e => setEmail(e.currentTarget.value)} required />
                                             <label className="form-label" htmlFor="signInEmail">Email address</label>
                                         </div>
 
                                         <div className="form-outline mb-4">
-                                            <input type="password" id="signInPassword" className="form-control" />
+                                            <input type="password" id="signInPassword" className="form-control" value={password} onChange={e => setPassword(e.currentTarget.value)} required />
                                             <label className="form-label" htmlFor="signInPassword">Password</label>
                                         </div>
 
-                                        <button type="submit" className="btn btn-primary btn-block mb-4">
+					<Button
+					    varient="primary"
+					    className="btn-block mb-4"
+					    type="submit"
+					>
                                             Sign In
-                                        </button>
+                                        </Button>
 
+					{/*
                                         <div className="text-center">
                                             <p>or sign in with:</p>
                                             <button type="button" className="btn btn-link btn-floating mx-1">
                                                 <FontAwesomeIcon icon={faGoogle} />
                                             </button>
-                                        </div>
+                                            </div>
+					 */}
                                     </form>
                                 </div>
                             </div>
