9cb71524fada59a8599805c85a1c56ecf3f1b769
Brett Kaplan <bkapla01@gmail.com>
2023-09-19 16:44:18 -0500

    Added confirm stage to updating users, changed route

--- frontend/src/pages/Admin.jsx
+++ frontend/src/pages/Admin.jsx
@@ -1,30 +1,94 @@
 
 import React, { useEffect, useState, useCallback } from 'react';
-import { Table, Button, Container } from 'react-bootstrap';
+import { Table, Button, Container, Modal } from 'react-bootstrap';
 
 import AdminNavbar from '../components/AdminNavbar'
 
 function Admin() {
+    const [origData, setOrigData] = useState(null);
     const [teachersData, setTeachersData] = useState(null);
     const [didChange, setDidChange] = useState(false);
 
-    const handleOnChange = useCallback(event => {
+    const handleOnChange = (event) => {
         const { name, value } = event.target;
         const index = event.target.parentElement.parentElement.dataset.index;
         const copy = [...teachersData]
         copy[index][name] = value;
         setTeachersData(copy);
         setDidChange(true);
-    });
+    };
 
-    const handlePressEnter = useCallback(event => {
+    const handlePressEnter = (event) => {
         if (event.keyCode === 13 || event.key === 'Enter') {
             event.target.blur();
             return false;
         }
-    });
+    };
+
+    const [showConfirm, setShowConfirm] = useState(false);
+    const [confirmEvent, setConfirmEvent] = useState(false);
+    const [changeProp, setChangeProp] = useState("");
+    const [changePropFrom, setChangePropFrom] = useState("");
+    const [changePropTo, setChangePropTo] = useState("");
+
+    const handleClose = () => setShowConfirm(false);
+    const handleShow = () => setShowConfirm(true);
+
+    const capitalizeFirst = (str) => {
+	return str.charAt(0).toUpperCase() + str.slice(1);
+    }
+
+    const handleBlurEditable = (e) => {
+        if (didChange) {
+	    setConfirmEvent(e);
+
+	    const { name } = e.target;
+            const index = e.target.parentElement.parentElement.dataset.index;
+
+	    setChangeProp(capitalizeFirst(name));
+	    setChangePropFrom(origData[index][name].toString());
+	    setChangePropTo(teachersData[index][name].toString());
+
+	    handleShow();
+	}
+    };
+
+    const handleCancel = () => {
+	const { name } = confirmEvent.target;
+        const index = confirmEvent.target.parentElement.parentElement.dataset.index;
+        const copy = [...teachersData]
+        copy[index][name] = origData[index][name];
+        setTeachersData(copy);
+
+	handleClose();
+    }
+
+    const sendUpdateRequest = () => {
+	const e = confirmEvent;
+	console.log('changed');
+	
+	const obj = {}
+	obj[e.target.name] = e.target.value;
+	
+	fetch("./adminPanel/updateUser/" + teachersData[e.target.parentElement.parentElement.dataset.index].id, {
+	    headers: {
+		Accept: "application/json",
+		"Content-Type": "application/json",
+	    },
+	    method: "PATCH",
+	    body: JSON.stringify(obj)
+	});
+
+	const { name } = confirmEvent.target;
+        const index = confirmEvent.target.parentElement.parentElement.dataset.index;
+        const copy = [...origData]
+        copy[index][name] = teachersData[index][name];
+        setOrigData(copy);
+
+	handleClose();
+    }
     
-    function editableCell(type, cellInfo) {
+    const editableCell = (type, cellInfo) => {
         return (
             <td>
                 <input 
@@ -34,57 +98,23 @@ function Admin() {
                     style={{ width: '100%', borderSizing: 'border-box', border: 'none', padding: '0.5rem' }}
                     data-starting={ cellInfo }
                     onChange={handleOnChange}
-                    onBlur={(e) => {
-                        if (didChange) {
-                            console.log('changed');
-
-                            // todo: send api request to update data
-                            // avaliable: teachersData[e.target.parentElement.parentElement.dataset.index].id,
-                            //            key: event.target.name, value: event.target.value
-
-			    const obj = {}
-			    obj[e.target.name] = e.target.value;
-
-			    fetch("./adminPanel/" + teachersData[e.target.parentElement.parentElement.dataset.index].id, {
-				headers: {
-				    Accept: "application/json",
-				    "Content-Type": "application/json",
-				},
-				method: "PATCH",
-				body: JSON.stringify(obj)
-			    })
-                        }
-                    }}
+                    onBlur={handleBlurEditable}
                     onFocus={(e) => { setDidChange(false); }}
                     onKeyDown={handlePressEnter}
                 />
             </td>
         );
     }
-
+    
     function loadTeachers() {
         setTeachersData(null);
         fetch("./adminPanel/data")
-          .then((response) => response.json())
-          .then((json) => setTeachersData(json));
-
-        /*
-        const array = [];
-        const firstNames = ["John", "Jane", "Jack", "Jill", "James", "Judy", "Jenny", "Jesse", "Jasmine", "Jasper", "Jade", "Jared", "Jocelyn", "Jude", "Jenna", "Javier"];
-        const lastNames = ["Doe", "Smith", "Johnson", "Williams", "Jones", "Brown", "Davis", "Miller", "Wilson", "Moore", "Taylor", "Anderson", "Thomas", "Jackson", "White", "Harris"];
-        for (let i = 0; i < 100; i++) {
-            // generate random data
-            const firstName = firstNames[Math.floor(Math.random() * firstNames.length)];
-            const lastName = lastNames[Math.floor(Math.random() * lastNames.length)];
-            array.push({
-                id: i + 1,
-                firstName: firstName,
-                lastName: lastName,
-                email: `${firstName.toLowerCase()}.${lastName.toLowerCase()}@allenisd.org`,
-            });
-        }
-        setTeachersData(array);
-        */
+            .then((response) => response.json())
+            .then((json) => {
+		setTeachersData(json);
+		const copy = JSON.parse(JSON.stringify(json));
+		setOrigData(copy);
+	    });
     }
 
     useEffect(() => {
@@ -118,7 +148,7 @@ function Admin() {
                                                 { editableCell('email', data.email) }
 
                                                 <td className='text-center'>
-                                                    <Button className="text-nowrap">Show Avaliability</Button>
+                                                    <Button className="text-nowrap">Show Availability</Button>
                                                 </td>
                                             </tr>
                                         );
@@ -129,6 +159,34 @@ function Admin() {
                     </Container>
                 </div>
             </section>
+
+	    <Modal
+		show={showConfirm}
+		onHide={handleCancel}
+		size="lg"
+		aria-labelledby="contained-modal-title-vcenter"
+		centered
+	    >
+		<Modal.Header closeButton>
+		    <Modal.Title id="contained-modal-title-vcenter" className="text-center">
+			Confirm User Update
+		    </Modal.Title>
+		</Modal.Header>
+		<Modal.Body className="text-center">
+		    <h4>{changeProp.replace("Name", " Name")}</h4>
+		    <p>
+			{changePropFrom} -> {changePropTo}
+		    </p>
+		</Modal.Body>
+		<Modal.Footer>
+		    <Button variant="danger" onClick={handleCancel}>
+			Cancel
+		    </Button>
+		    <Button variant="primary" onClick={sendUpdateRequest}>
+			Confirm
+		    </Button>
+		</Modal.Footer>
+	    </Modal>
         </main>
     );
 }
