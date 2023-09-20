a33a69ce353743193e220178f8f17abbc7103712
Brett Kaplan <bkapla01@gmail.com>
2023-09-20 14:39:48 -0500

    Added admin to navbar

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,14 +1,30 @@
+import { useEffect, useState } from 'react';
+
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 
 function NavbarComponent() {
+    const [showAdmin, setShowAdmin] = useState(false);
+    
+    useEffect(() => {
+	fetch("./userPermission", {
+	    method: "GET"
+	}).then(response => {
+	    setShowAdmin(!!response)
+	});
+    });
+    
   return (
     <Navbar bg="primary" data-bs-theme="dark">
         <Container>
             <Navbar.Brand href="./dashboard">AHS DutyDashboard</Navbar.Brand>
             <Nav className="me-auto">
                 <Nav.Link href="#">Home</Nav.Link>
+            </Nav>
+
+	    <Nav className="ml-auto" style={{display: showAdmin ? "block" : "none"}}>
+                <Nav.Link href="./adminPanel">Admin</Nav.Link>
             </Nav>
         </Container>
     </Navbar>
