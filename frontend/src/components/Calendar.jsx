b62d75dccb174b3f8060a3cf544d59fd489155a4
Brett Kaplan <bkapla01@gmail.com>
2023-10-28 07:36:16 -0500

    Added Events to calendar, experiencing error with backend

--- frontend/src/components/Calendar.jsx
+++ frontend/src/components/Calendar.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useState, useRef } from 'react'
 import FullCalendar from '@fullcalendar/react'
 import dayGridPlugin from '@fullcalendar/daygrid' // a plugin!
 
@@ -8,24 +8,25 @@ function Calendar() {
 
     const [didRequest, setDidRequest] = useState("");
     const [dateInfo, setDateInfo] = useState([]);
+    const calendarRef = useRef();
 
     /**
-     * @param {DayCellContentArg} info
+     * @param {[]} eventsData
      */
-    const colorAorB = (date) => {
-        if ([0, 6].includes(date.getDay())) return "black";
-        if (date.getDate() % 2 === 0) return "#c03a2a";
-        return "#161b5f";
+    const updateEvents = (eventsData) => {
+        console.log(eventsData);
+        const api = calendarRef.current.getApi();
+        eventsData.forEach(e => {
+            api.addEvent(e);
+        });
     }
 
     /**
      * @param {DayCellContentArg} info
      */
     const updateDayColor = (info) => {
-        // will eventually use dateInfo object to determine color
-        // for now, just use colorAorB
         return (
-            <span style={{ color: "gray"}} id={`dayNum-${info.date.toISOString().split('T')[0]}`}>
+            <span style={{ color: "gray" }} id={`dayNum-${info.date.toISOString().split('T')[0]}`}>
                 {info.dayNumberText}
             </span>
         );
@@ -38,8 +39,7 @@ function Calendar() {
             const date = new Date(start);
             const dayNum = document.getElementById(`dayNum-${date.toISOString().split('T')[0]}`);
             if (dayNum) {
-		const dayInfo = dateInfo.find(a => a.day.split("T")[0] === date.toISOString().split("T")[0]);
-		console.log(dayInfo);
+                const dayInfo = dateInfo.find(a => a.day.split("T")[0] === date.toISOString().split("T")[0]);
                 dayNum.style.color = dayInfo ? (dayInfo.supplementaryJSON.abDay === "A" ? "#c03a2a" : "#161b5f") : "gray";
             }
             start.setDate(start.getDate() + 1);
@@ -47,34 +47,53 @@ function Calendar() {
     }
 
     const getData = (info) => {
-	if (didRequest === info.start.toISOString() + "+" + info.end.toISOString()) return;
+        if (didRequest === info.start.toISOString() + "+" + info.end.toISOString()) return;
 
-	setDidRequest(info.start.toISOString() + "+" + info.end.toISOString());
-	try {
-        // make http request to get data
-        fetch(process.env.PUBLIC_URL + "/calendar/data",
-              {
-		  headers: {
-		      'Accept': 'application/json',
-		      "Content-Type": "application/json",
-		  },
-		  method: "POST",
-		  body: JSON.stringify({from: info.start.toISOString().split("T")[0]+"T00:00:00Z", through: info.end.toISOString().split(".")[0]+"Z"})
-              })
-            .then((response) => response.json())
-            .then((json) => {
-		setDateInfo(json);
-		updateDayColors(info, json);
-            });
-	} catch(err) {
-	    console.error(err);
-	}
+        setDidRequest(info.start.toISOString() + "+" + info.end.toISOString());
+        try {
+            // make http request to get data
+            fetch(process.env.PUBLIC_URL + "/calendar/data",
+                {
+                    headers: {
+                        'Accept': 'application/json',
+                        "Content-Type": "application/json",
+                    },
+                    method: "POST",
+                    body: JSON.stringify({ from: info.start.toISOString().split("T")[0] + "T00:00:00Z", through: info.end.toISOString().split(".")[0] + "Z" })
+                })
+                .then((response) => response.json())
+                .then((json) => {
+                    setDateInfo(json);
+                    updateDayColors(info, json);
+                });
+        } catch (err) {
+            console.error(err);
+        }
+        try {
+            // make http request to get data
+            fetch(process.env.PUBLIC_URL + "/duties/user/date",
+                {
+                    headers: {
+                        'Accept': 'application/json',
+                        "Content-Type": "application/json",
+                    },
+                    method: "POST",
+                    body: JSON.stringify({ from: info.start.toISOString().split("T")[0] + "T00:00:00Z", through: info.end.toISOString().split(".")[0] + "Z" })
+                })
+                .then((response) => response.json())
+                .then((json) => {
+                    updateEvents(json);
+                });
+        } catch (err) {
+            console.error(err);
+        }
     }
-    
+
     return (
         <FullCalendar
+            ref={calendarRef}
             themeSystem='bootstrap5'
-            plugins={[ dayGridPlugin ]}
+            plugins={[dayGridPlugin]}
             initialView="dayGridMonth"
             datesSet={getData}
             dayCellContent={updateDayColor}
