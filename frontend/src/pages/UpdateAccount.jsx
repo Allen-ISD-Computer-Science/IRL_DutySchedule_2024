993007677316396e49ed51a6e6c45dac686df8bf
Ryan Hallock <ryan.hallock@student.allenisd.org>
2024-02-14 10:22:24 -0600

    Fix compile error.

--- frontend/src/pages/UpdateAccount.jsx
+++ frontend/src/pages/UpdateAccount.jsx
@@ -32,10 +32,10 @@ function UpdateAccount() {
         }
         else if (data.error){
             if (data.error === "Account successfully created."){
-                window.location.href = {process.env.PUBLIC_URL + "../signin"};
+                window.location.href = process.env.PUBLIC_URL + "../signin";
             }
             else if (data.error === "Account already verified."){
-                window.location.href = {process.env.PUBLIC_URL + "../signin"};
+                window.location.href = process.env.PUBLIC_URL + "../signin";
             }
             else {
                 alert(data.error)
